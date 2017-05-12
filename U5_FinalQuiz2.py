3513dc7f94eeade4004da05b389948d9e785cf43
GentFromPhoenix <tchesebrough@ucansustain.com>
2017-05-12 16:20:05 -0700

    Unit5-Quiz - Modified Hashtable(s) to eliminate repeating code
    Modified hashtable_update and hashtable_lookup to call another
    procedure that performs the for loops

--- U5_FinalQuiz2.py
+++ U5_FinalQuiz2.py
@@ -5,20 +5,30 @@
 
 # Here are the original procedures:
 
+def bucket_find(bucket,key):
+    for entry in bucket:
+        if entry[0] == key:
+            return entry
+    return None   
+            
+            
+    
+
 def hashtable_update(htable, key, value):
     bucket = hashtable_get_bucket(htable, key)
-    for entry in bucket:
-        if entry[0] == key:
-            entry[1] = value
-            return
-    bucket.append([key, value])
+    entry = bucket_find(bucket,key)
+    if entry:
+        entry[1] = value
+    else:
+        bucket.append([key, value])    
 
 def hashtable_lookup(htable, key):
-    bucket = hashtable_get_bucket(htable, key)
-    for entry in bucket:
-        if entry[0] == key:
-            return entry[1]
-    return None
+    entry = bucket_find(hashtable_get_bucket(htable, key),key)
+    if entry:
+        return entry[1]
+    else:
+        return None
+    
 
 def make_hashtable(size):
     table = []
